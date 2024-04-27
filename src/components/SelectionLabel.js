87ca87c459a2996be35295b7a22ad16a5ec4e639
Manju Yadav <manjuyadav@Manjus-iMac.home>
2024-04-26 23:57:01 -0400

    added home dir

--- src/components/SelectionLabel.js
+++
@@ -1,8 +0,0 @@
-// components/SelectionLabel.js
-import React from 'react';
-
-function SelectionLabel({ text }) {
-  return <div className="selection-label">{text}</div>;
-}
-
-export default SelectionLabel;
